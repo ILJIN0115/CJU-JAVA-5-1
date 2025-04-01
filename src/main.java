3c13800cbf1ef3b4c760259f9bffcf7d5ffb4e95
CJU-PC <prue0115@gmail.com>
2025-04-01 10:49:55 +0900

    2025-04-01 1117

---
+++ src/main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+        int num, num2, num3 = 0;
+
+        System.out.print("숫자를 입력하시오;");
+        num = sc.nextInt();
+        System.out.print("한번 더 입력해;");
+        num2 = sc.nextInt();
+        System.out.print("한번 더 입력해;");
+        num3 = sc.nextInt();
+
+        System.out.println(num-num2-num3);
+    }
+}
